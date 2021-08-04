1e2b5f9b85cf6c56918ddb822bbc7edeca9d1696
cytusalive <cytusomega@gmail.com>
2021-08-03 19:23:33 -0700

    daylistitem component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,24 +2,22 @@ import React from "react";
 
 import "components/Button.scss";
 
-export default function Button(props) {
-   let buttonClass = "button";
- 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
-   
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+var classnames = require('classnames');
 
+export default function Button(props) {
+   const buttonClass = classnames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+ 
    return (
-      <button
-        className={buttonClass}
-        onClick={props.onClick}
-        disabled={props.disabled}
-      >
-        {props.children}
-      </button>
-    );
-}
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
+ 
---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/DayListItem.scss"
+var classnames = require('classnames');
+
+export default function DayListItem(props) {
+  const formatSpots = () => { // props.spot is #
+    if (props.spots === 0) {
+      return "no spots remaining"
+    } else if (props.spots === 1) {
+      return `${props.spots} spot remaining`
+    } else if (props.spots >= 2) {
+      return `${props.spots} spots remaining`
+    }
+  }
+
+  const dayClass = classnames("day-list", {
+    'day-list__item': true,
+    'day-list__item--selected': props.selected,
+    "day-list__item--full": props.spots===0 ? true:false
+  })
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name} </h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
