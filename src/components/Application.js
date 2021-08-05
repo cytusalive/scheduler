90c402241dcc67fa80a97cba4f9e5a48e41cc427
cytusalive <cytusomega@gmail.com>
2021-08-05 02:59:22 -0700

    add appointment data to application

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
-
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -22,12 +22,62 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "John T",
+      interviewer: {
+        id: 4, 
+        name: "Cohana Roy", 
+        avatar: "https://i.imgur.com/FK8V841.jpg"
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Cytus A",
+      interviewer: { 
+        id: 5, 
+        name: "Sven Jones", 
+        avatar: "https://i.imgur.com/twYrpay.jpg" 
+      }
+    }
+  },
+  {
+    id: "last",
+    time: "5pm",
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
@@ -47,8 +97,13 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      <section className="schedule"> {
+        appointments.map((appointment) =>
+        <
+          Appointment key={appointment.id} 
+          {...appointment} 
+        />
+        )}
       </section>
     </main>
 
