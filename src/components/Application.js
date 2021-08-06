8eacfb31626f5e91a9bc65342930c09d375d6c77
cytusalive <cytusomega@gmail.com>
2021-08-05 21:25:10 -0700

    using axios to get data instead

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+const [days, setDays] = useState([]);
 
 const appointments = [
   {
@@ -75,6 +60,14 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('api/days')
+    .then(res => setDays(res.data))
+    .catch(err => console.log(err))
+  },[])
+
   return (
     <main className="layout">
       <section className="sidebar">
