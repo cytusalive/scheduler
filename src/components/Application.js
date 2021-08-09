9e7925a6f0c7d8e78a3016666424bac14f37aaef
cytusalive <cytusomega@gmail.com>
2021-08-08 17:54:48 -0700

    selectors functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,18 +6,21 @@ import DayList from "./DayList";
 
 import "components/Application.scss";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
+
   const [state, setState] = useState({
     day: "Monday", 
     days: [], 
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
+
   let dailyAppointments = [];
-  
+
   useEffect(() => {
     const GET_DAYS = '/api/days';
     const GET_APPOINTMENTS = '/api/appointments';
@@ -31,7 +34,8 @@ export default function Application(props) {
       setState(prev => ({
         ...prev,
         days: all[0].data,
-        appointments: all[1].data
+        appointments: all[1].data,
+        interviewers: all[2].data
       }))
     })
     
@@ -39,10 +43,17 @@ export default function Application(props) {
   
   dailyAppointments = getAppointmentsForDay(state, state.day); 
 
-  const appointmentItem = dailyAppointments
-    .map(appointment => {
-      return <Appointment key={appointment.id} {...appointment}/>
-    });
+  const appointmentItem = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment 
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview} 
+      />
+    )
+  });
   
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,7 +4,6 @@ export function getAppointmentsForDay(state, day) {
   if(state.days.length === 0){
     return output;
   }
-  // Filter out the day objects equal to the passed 'day' variable.
   const filteredDay = state.days.filter(data => data.name === day)
 
   if(Object.keys(filteredDay).length) {
@@ -13,4 +12,23 @@ export function getAppointmentsForDay(state, day) {
   }
 
   return output;
+} 
+
+export function getInterview(state, interview) {
+
+  if (!interview){
+    return null;
+  }
+
+  let filteredInterview;
+  let filteredInterviewer;
+  for (const key in state.appointments) {
+    if(state.appointments[key].interview !== null && state.appointments[key].interview.interviewer === interview.interviewer) {
+      filteredInterview = state.appointments[key].interview; 
+      filteredInterviewer = interview.interviewer; 
+      filteredInterview['interviewer'] = state.interviewers[filteredInterviewer]; 
+    }
+  }
+
+  return filteredInterview;
 } 
