05d80bfec654aad623dc54d25c44862c7fb70060
cytusalive <cytusomega@gmail.com>
2021-08-12 03:42:00 -0700

    rewriting

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,26 +1,29 @@
-import React from "react";
-import InterviewerListItem from "./InterviewerListItem";
-import "components/InterviewerList.scss"
+import React from 'react';
+import InterviewerListItem from './InterviewerListItem';
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
-  const { interviewers } = props;
-  
-  const interviewerItem = interviewers.map(interviewer => {
-    return (
-      <InterviewerListItem
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        setInterviewer={event => props.setInterviewer(interviewer.id)}
-      />
-    );
-  });
-  
+import 'components/InterviewerList.scss';
+
+const InterviewerList = ({ interviewers, value, onChange }) => {
   return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewerItem}</ul>
+    <section className='interviewers'>
+      <h4 className='interviewers__header text--light'>Interviewer</h4>
+      <ul className='interviewers__list'>
+        {interviewers.map((teacher) => (
+          <InterviewerListItem
+            {...teacher}
+            key={teacher.id}
+            setInterviewer={() => onChange(teacher.id)}
+            selected={teacher.id === value}
+          />
+        ))}
+      </ul>
     </section>
-  )
-} 
+  );
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
